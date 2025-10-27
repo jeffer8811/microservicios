75f77a16b2f09bdc795fe5958074c7a1cb04d43d
Jeffer <73095499@certus.edu.pe>
2025-10-27 12:31:26 -0500

    Subiendo proyecto de microservicios sin JARs

--- academico/src/main/java/com/microservicios/academico/AcademicoApplication.java
+++ academico/src/main/java/com/microservicios/academico/AcademicoApplication.java
@@ -2,8 +2,10 @@ package com.microservicios.academico;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class AcademicoApplication {
 
 	public static void main(String[] args) {
--- matricula/src/main/java/com/microservicios/matricula/MatriculaApplication.java
+++ matricula/src/main/java/com/microservicios/matricula/MatriculaApplication.java
@@ -2,12 +2,13 @@ package com.microservicios.matricula;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class MatriculaApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(MatriculaApplication.class, args);
-	}
-
+	
+    public static void main(String[] args) {
+        SpringApplication.run(MatriculaApplication.class, args);
+    }
 }

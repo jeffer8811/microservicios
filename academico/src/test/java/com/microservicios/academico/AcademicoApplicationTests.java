3458ba807a4674acc22a563d7aa7b4a75987cb21
Jeffer <73095499@certus.edu.pe>
2025-10-27 10:45:06 -0500

    microservicios academico, matricula, reporte y usuario con Docker

---
+++ academico/src/test/java/com/microservicios/academico/AcademicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicios.academico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ reporte/src/test/java/com/microservicios/reporte/ReporteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicios.reporte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReporteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
